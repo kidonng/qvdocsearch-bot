83612851a2dfef4d4d1b774b88c22388cc8afff2
Kid <kidonng@gmail.com>
2020-09-03 20:33:49 +0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Telegraf from 'telegraf'
+import { docsearch } from './docsearch'
+import { IContext, request } from './utils'
+
+const { BOT_TOKEN, IS_VERCEL } = process.env
+
+if (!BOT_TOKEN)
+  throw Error('Please provide BOT_TOKEN as an environment variable')
+
+export const telegraf = new Telegraf<IContext>(BOT_TOKEN)
+
+docsearch(telegraf)
+
+telegraf.on('message', ({ telegram }) => {
+  // Workaround for skipping unhandled messages
+  // https://github.com/telegraf/telegraf/issues/1089
+  if (telegram.webhookReply) {
+    request.handler!.status(200).end()
+  }
+})
+
+if (!IS_VERCEL) telegraf.launch()
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+import { Telegraf, Context } from 'telegraf/typings'
+import { ExtraEditMessage } from 'telegraf/typings/telegram-types'
+import { MiddlewareFn } from 'telegraf/typings/composer'
+import { NowResponse } from '@vercel/node'
+
+export interface IContext extends Context {
+  replyWithMarkdownV2(
+    markdown: string,
+    extra?: ExtraEditMessage
+  ): MiddlewareFn<IContext>
+}
+
+export interface Component {
+  (telegraf: Telegraf<IContext>): void
+}
+
+// https://stackoverflow.com/a/60145565
+export const escape = (text: string) =>
+  text.replace(/(\[[^\][]*]\(http[^()]*\))|[[\]()>#+\-=|{}.!]/gi, (x, y) =>
+    y ? y : '\\' + x
+  )
+
+export const request: { handler?: NowResponse } = {}
