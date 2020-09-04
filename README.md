305764a28d557c1d04b1445832df68eb055e9484
Kid <kidonng@gmail.com>
2020-09-04 13:23:37 +0800

    Make it universal

---
+++ README.md
@@ -0,0 +1,24 @@
+# 🔍 [Qv2ray](https://github.com/Qv2ray) DocSearch bot
+
+Source code for Telegram bot [@Qvdocsearchbot](https://t.me/Qvdocsearchbot), an inline bot for searching [Qv2ray documentation](https://qv2ray.net) using [DocSearch](https://docsearch.algolia.com/). It can also be configured to search any website using DocSearch.
+
+## Setup
+
+The following environment variables should be provided:
+
+- `BOT_TOKEN`: Telegram bot token. The bot should be added to your **public** channel first. [Click here](http://t.me/BotFather) to create a bot.
+- `ALGOLIA_API_KEY`: DocSearch API key.
+- `ALGOLIA_INDEX_NAME`: DocSearch index name.
+
+You should also change or remove [the search result filter](https://github.com/kidonng/qvdocsearch-bot/blob/820373e8b669d31d623d2de74c5f0bb0b25899c7/src/docsearch.ts#L21).
+
+### Using [Vercel](http://vercel.com/) and Webhooks (recommended)
+
+1. Configure the environment variables in [`vercel.json`](vercel.json).
+2. [Deploy to Vercel](https://vercel.com/import/project?template=http://github.com/kidonng/qvdocsearch-bot).
+3. [Set up the Webhook](https://core.telegram.org/bots/api#setwebhook): `https://api.telegram.org/bot<token>/setWebhook?url=<url>` (**Make sure to use a private URL!**)
+
+### Using polling
+
+1. Configure the environment variables.
+2. Run `src/index.ts` to launch the bot.
--- src/docsearch.ts
+++ src/docsearch.ts
@@ -3,19 +3,33 @@ import { InlineQueryResultArticle } from 'telegram-typings'
 import outdent from 'outdent'
 import { Component, escape } from './utils'
 
-// appID: https://github.com/algolia/docsearch/blob/7c1d293af9b828cf2553d7ea61c54eb78f4dc882/src/lib/DocSearch.js#L36
-const client = algoliasearch('BH4D9OD16A', 'f0da09ddb339a1b1fabe6ac3fbd78f42')
-const index = client.initIndex('qv2ray')
-
 export const docsearch: Component = (telegraf) => {
   telegraf.on('inline_query', async (ctx) => {
-    const { id, query } = ctx.inlineQuery!
+    const { ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME } = process.env
+    if (!ALGOLIA_API_KEY || !ALGOLIA_INDEX_NAME) {
+      const message_text = 'No ALGOLIA_API_KEY or ALGOLIA_INDEX_NAME provided'
+
+      return ctx.answerInlineQuery([
+        {
+          type: 'article',
+          id: '0',
+          title: message_text,
+          input_message_content: {
+            message_text,
+          },
+        },
+      ])
+    }
+
+    // appID: https://github.com/algolia/docsearch/blob/7c1d293af9b828cf2553d7ea61c54eb78f4dc882/src/lib/DocSearch.js#L36
+    const client = algoliasearch('BH4D9OD16A', ALGOLIA_API_KEY)
+    const index = client.initIndex(ALGOLIA_INDEX_NAME)
 
     const { hits } = await index.search<{
       hierarchy: Record<string, string | null>
       content: string | null
       url: string
-    }>(query)
+    }>(ctx.inlineQuery!.query)
 
     const results: InlineQueryResultArticle[] = hits
       .filter((hit) => !hit.url.includes('qv2ray.net/en'))
@@ -42,6 +56,6 @@ export const docsearch: Component = (telegraf) => {
         }
       })
 
-    ctx.telegram.answerInlineQuery(id, results)
+    ctx.answerInlineQuery(results)
   })
 }
